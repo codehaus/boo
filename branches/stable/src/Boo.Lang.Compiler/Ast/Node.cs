3126c5f97eb2d13d3ff6bb7953bee4fb47c3315e
bamboo <bamboo@2c1201b4-01cd-e047-a400-b836ae1fbc61>
2005-07-06 13:14:56 +0000

    stable branch update

---
+++ branches/stable/src/Boo.Lang.Compiler/Ast/Node.cs
@@ -0,0 +1,256 @@
+﻿#region license
+// Copyright (c) 2004, Rodrigo B. de Oliveira (rbo@acm.org)
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+// 
+//     * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//     * Neither the name of Rodrigo B. de Oliveira nor the names of its
+//     contributors may be used to endorse or promote products derived from this
+//     software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+namespace Boo.Lang.Compiler.Ast
+{
+	using System;
+	using System.Collections;
+	using System.Xml.Serialization;
+
+	/// <summary>
+	/// Base class for every node in the AST.
+	/// </summary>
+	[Serializable]
+	public abstract class Node : ICloneable
+	{
+		protected LexicalInfo _lexicalInfo = LexicalInfo.Empty;
+		
+		protected SourceLocation _endSourceLocation = LexicalInfo.Empty;
+
+		protected Node _parent;
+		
+		protected string _documentation;
+		
+		protected Boo.Lang.Compiler.TypeSystem.IEntity _entity;
+		
+		protected System.Collections.Hashtable _annotations;
+		
+		protected bool _isSynthetic;
+
+		protected Node()
+		{
+			_lexicalInfo = LexicalInfo.Empty;
+		}
+
+		protected Node(LexicalInfo lexicalInfo)
+		{
+			if (null == lexicalInfo)
+			{
+				throw new ArgumentNullException("lexicalInfo");
+			}
+			_lexicalInfo = lexicalInfo;
+		}
+
+		protected void InitializeFrom(Node other)
+		{
+			_lexicalInfo = other.LexicalInfo;
+		}
+		
+		public Node CloneNode()
+		{
+			return (Node)Clone();
+		}
+		
+		/// <summary>
+		/// true when the node was constructed by the compiler.
+		/// </summary>
+		[XmlAttribute]
+		[System.ComponentModel.DefaultValue(false)]
+		public bool IsSynthetic
+		{
+			get
+			{
+				return _isSynthetic;
+			}
+			
+			set
+			{
+				_isSynthetic = value;
+			}
+		}
+		
+		[XmlIgnore]
+		public Boo.Lang.Compiler.TypeSystem.IEntity Entity
+		{
+			get
+			{
+				return _entity;
+			}
+			
+			set
+			{
+				_entity = value;
+			}
+		}
+		
+		public object this[object key]
+		{
+			get
+			{
+				if (null == _annotations)
+				{
+					return null;
+				}
+				return _annotations[key];
+			}
+			
+			set
+			{
+				if (null == key)
+				{
+					throw new ArgumentNullException("key");
+				}
+				
+				if (null == _annotations)
+				{
+					_annotations = new Hashtable();
+				}
+				_annotations[key] = value;
+			}
+		}
+		
+		public bool ContainsAnnotation(object key)
+		{
+			return (null == _annotations)
+				? false
+				: _annotations.ContainsKey(key);
+		}
+		
+		public void RemoveAnnotation(object key)
+		{
+			if (null != _annotations)
+			{
+				_annotations.Remove(key);
+			}
+		}
+		
+		public Node ParentNode
+		{
+			get
+			{
+				return _parent;
+			}
+		}
+		
+		public string Documentation
+		{
+			get
+			{
+				return _documentation;
+			}
+			
+			set
+			{
+				_documentation = value;
+			}
+		}
+
+		[XmlIgnore]
+		public LexicalInfo LexicalInfo
+		{
+			get
+			{	
+				return _lexicalInfo;
+			}
+
+			set
+			{
+				if (null == value)
+				{
+					throw new ArgumentNullException("LexicalInfo");
+				}
+				_lexicalInfo = value;
+			}
+		}
+		
+		/// <summary>
+		/// Where this element ends in the source file.
+		/// This information is generally available and/or accurate
+		/// only for blocks and type definitions.
+		/// </summary>
+		[System.Xml.Serialization.XmlIgnore]
+		public virtual SourceLocation EndSourceLocation
+		{
+			get
+			{
+				return _endSourceLocation;
+			}
+			
+			set
+			{
+				if (null == value)
+				{
+					throw new ArgumentNullException("EndSourceLocation");
+				}
+				_endSourceLocation = value;
+			}
+		}
+		
+		public virtual bool Replace(Node existing, Node newNode)
+		{
+			if (null == existing)
+			{
+				throw new ArgumentNullException("existing");
+			}
+			return false;
+		}
+
+		internal void InitializeParent(Node parent)
+		{			
+			_parent = parent;
+		}
+
+		public abstract void Accept(IAstVisitor visitor);
+		
+		public abstract object Clone();
+		
+		public virtual void ClearTypeSystemBindings()
+		{
+			_annotations = null;
+			_entity = null;
+		}
+		
+		public abstract NodeType NodeType
+		{
+			get;
+		}
+		
+		override public string ToString()
+		{
+			return ToCodeString();
+		}
+		
+		public string ToCodeString()
+		{
+			System.IO.StringWriter writer = new System.IO.StringWriter();
+			new Visitors.BooPrinterVisitor(writer).Visit(this);
+			return writer.ToString();
+		}
+	}
+
+}
---
+++ branches/stable/src/Boo.Lang.Compiler/Steps/BindNamespaces.cs
@@ -0,0 +1,94 @@
+﻿#region license
+// Copyright (c) 2004, Rodrigo B. de Oliveira (rbo@acm.org)
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//     * Neither the name of Rodrigo B. de Oliveira nor the names of its
+//     contributors may be used to endorse or promote products derived from this
+//     software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+using System.Reflection;
+using Boo.Lang.Compiler.Ast;
+using Boo.Lang.Compiler;
+using Boo.Lang.Compiler.TypeSystem;
+
+namespace Boo.Lang.Compiler.Steps
+{
+	public class BindNamespaces : AbstractCompilerStep
+	{
+		override public void Run()
+		{
+			NameResolutionService.Reset();
+			
+			foreach (Boo.Lang.Compiler.Ast.Module module in CompileUnit.Modules)
+			{
+				foreach (Import import in module.Imports)
+				{
+					IEntity entity = NameResolutionService.ResolveQualifiedName(import.Namespace);
+					if (null == entity)
+					{
+						Errors.Add(CompilerErrorFactory.InvalidNamespace(import));
+						entity = TypeSystemServices.ErrorEntity;
+					}
+					else
+					{
+						if (!IsValidNamespace(entity))
+						{
+							Errors.Add(CompilerErrorFactory.NotANamespace(import, entity.FullName));
+							entity = TypeSystemServices.ErrorEntity;
+						}
+						else
+						{
+							if (null != import.AssemblyReference)
+							{
+								NamespaceEntity nsInfo = entity as NamespaceEntity;
+								if (null != nsInfo)
+								{
+									entity = new AssemblyQualifiedNamespaceEntity(GetBoundAssembly(import.AssemblyReference), nsInfo);
+								}
+							}
+							if (null != import.Alias)
+							{
+								entity = new AliasedNamespace(import.Alias.Name, entity);
+								import.Alias.Entity = entity;
+							}
+						}
+					}
+					
+					_context.TraceInfo("{1}: import reference '{0}' bound to {2}.", import, import.LexicalInfo, entity.Name);
+					import.Entity = entity;
+				}
+			}
+		}
+		
+		private bool IsValidNamespace(IEntity entity)
+		{
+			EntityType type = entity.EntityType;
+			return type == EntityType.Namespace || type == EntityType.Type;
+		}
+		
+		Assembly GetBoundAssembly(ReferenceExpression reference)
+		{
+			return ((AssemblyReference)TypeSystemServices.GetEntity(reference)).Assembly;
+		}
+	}
+}
