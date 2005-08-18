ab9564c20628e063d1436c04ac62b38fde057e0b
bamboo <bamboo@2c1201b4-01cd-e047-a400-b836ae1fbc61>
2005-08-18 17:01:38 +0000

    connect outline to the current document

---
+++ booclipse/trunk/booclipse.ui/src/booclipse/ui/editors/BooDocument.java
@@ -0,0 +1,33 @@
+package booclipse.ui.editors;
+
+import org.eclipse.jface.text.Document;
+
+import booclipse.core.outline.OutlineNode;
+
+public class BooDocument extends Document {
+	
+	public interface OutlineListener {
+		void outlineChanged(OutlineNode newOutline);
+	}
+
+	private OutlineNode _outline = new OutlineNode();
+	private OutlineListener _listener;
+	
+	public BooDocument() {
+	}
+	
+	public OutlineNode getOutline() {
+		return _outline;
+	}
+
+	public void updateOutline(OutlineNode outline) {
+		_outline = outline;
+		if (null != _listener) {
+			_listener.outlineChanged(_outline);
+		}
+	}
+
+	public void addOutlineListener(OutlineListener listener) {
+		_listener = listener;
+	}
+}
