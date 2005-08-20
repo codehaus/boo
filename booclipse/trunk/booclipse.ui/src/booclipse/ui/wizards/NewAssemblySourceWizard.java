7df090ecc2c41a33ff3c4b74833b1526d7c0fa38
bamboo <bamboo@2c1201b4-01cd-e047-a400-b836ae1fbc61>
2005-08-20 21:19:44 +0000

    warnings removed

--- booclipse/trunk/booclipse.ui/src/booclipse/ui/wizards/NewAssemblySourceWizard.java
+++ booclipse/trunk/booclipse.ui/src/booclipse/ui/wizards/NewAssemblySourceWizard.java
@@ -1,13 +1,11 @@
 package booclipse.ui.wizards;
 
 import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
